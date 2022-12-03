0c9ce9755054c84aea37bbb385f0859df837aca1
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2022-12-03 18:47:42 +0100

    Added `Data.utf8String`.

--- Sources/Data-utf8String/Data_utf8String.swift
+++ Sources/Data-utf8String/Data_utf8String.swift
@@ -1,6 +1,23 @@
-public struct Data_utf8String {
-    public private(set) var text = "Hello, World!"
+//
+//  Data_utf8String.swift
+//  
+//
+//  Created by Jeremy Bannister on 12/3/22.
+//
 
-    public init() {
+///
+@_exported import Foundation
+
+///
+public extension Data {
+    
+    /// Attempts to decode the `Data` as a `String` using `String.Encoding.utf8`.
+    ///
+    /// This is just a handy shortcut for the slightly more verbose implementation:
+    /// ```
+    /// String(data: self, encoding: .utf8)
+    /// ```
+    var utf8String: String? {
+        String(data: self, encoding: .utf8)
     }
 }
---
+++ Tests/Data-utf8String-tests/Data_utf8String_tests.swift
@@ -0,0 +1,32 @@
+//
+//  Data_utf8String_tests.swift
+//  
+//
+//  Created by Jeremy Bannister on 12/3/22.
+//
+
+///
+import Data_utf8String
+import XCTest
+
+///
+final class Data_utf8String_tests: XCTestCase {
+    
+    ///
+    func test () {
+        
+        ///
+        func test (_ value: String) {
+            XCTAssertEqual(
+                value
+                    .data(using: .utf8)!
+                    .utf8String,
+                value
+            )
+        }
+        
+        test("hello")
+        test("")
+        test("🥹")
+    }
+}
